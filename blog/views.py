95d8c03c3c9e634526bbcbe1a531dec4ea39ece8
sangeun <sangeun99@khu.ac.kr>
2023-05-23 12:08:02 +0900

    new poem page test

--- blog/views.py
+++ blog/views.py
@@ -39,3 +39,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def poem_detail(request):
+    return render(request, 'blog/poem_detail.html', {})
